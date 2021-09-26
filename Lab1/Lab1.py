0c6debfcfc1fb5034210245ccaa2bf5f90e026f2
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2021-09-26 14:01:33 +0300

    unfinished script

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,3 +1,13 @@
+# -*- coding: UTF-8 -*-
+#!/Users/qwysam/.pyenv/shims/python3
+import sys
 import mtranslate
 from mtranslate import translate
-print(translate("Тестовый текст, наверное","uk","ru"))
+class Translator:
+    def Translate(text,tolanguage,fromlanguage):
+        return translate(text, tolanguage,fromlanguage)
+class Parser:
+    def Parse():
+        url = sys.argv[1]
+TranslatorObj = Translator()
+print(TranslatorObj.Translate("Тестируем методы Питона","en",sys.argv[2]))
